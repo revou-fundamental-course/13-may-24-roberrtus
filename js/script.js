b85c574f61d1d4f4042531fe68d15fb2c6e7ee1b
roberrtus <playaov23@gmail.com>
2024-05-23 09:06:23 +0700

    mencari js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-// ini js 
+  
+const Alas = document.getElementById('Alas')
+const Tinggi = document.getElementById('Tinggi')
+const Hitung = document.getElementById('Hitung')
+const output = document.getElementById('output')
+
+Hitungitung.addEventListener('click' , function() {
+    let a = Alas.value
+    let t = Tinggi value
+    let l - 0.5*a*t
+    output innerHTML = ` Luas segitiga dari alas ${a} cm dan tinggi ${t} cm adalah ${l} cm2
+})
